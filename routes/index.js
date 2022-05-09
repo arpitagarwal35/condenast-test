0793c532546595dd1b7704ae78409b07446f4a99
Arpit Agrawal <arpitagarwal35@gmail.com>
2022-05-09 19:09:13 +0530

    view & search news feature added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var moment = require('moment-timezone');
+var _ = require('lodash');
+
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
+
+router.get('/', function (req, res, next) {
+  var locals = {
+    moment: moment
+  };
+
+  if (!req.query || _.isEmpty(req.query)) {
+    newsapi.v2.topHeadlines({
+      language: 'en',
+      country: 'in'
+    }).then(response => {
+      locals.title = 'World Wide latest news';
+      locals.response = response;
+      res.render('index', locals);
+    });
+  } else {
+    newsapi.v2.everything({
+      q: req.query.q || "",
+      sources: 'bbc-news,the-verge',
+      domains: 'bbc.co.uk, techcrunch.com',
+      from: '2022-04-10',
+      to: '2022-05-08',
+      language: 'en',
+      sortBy: 'relevancy',
+      page: 2
+    }).then(response => {
+      locals.title = 'Search result';
+      locals.response = response;
+      locals.query = req.query || {};
+      res.render('index', locals);
+    });
+  }
 });
 
 module.exports = router;
